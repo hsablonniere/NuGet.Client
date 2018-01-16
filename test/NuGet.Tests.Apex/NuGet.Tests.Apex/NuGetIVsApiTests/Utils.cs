a8cf0c0029736a2940b8b9fc8661416da2f6c923
Justin Emgarten <justin@emgarten.com>
2018-01-16 10:48:56 -0800

    Improve IsPackageInstalled robustness

--- test/NuGet.Tests.Apex/NuGet.Tests.Apex/NuGetIVsApiTests/Utils.cs
+++ test/NuGet.Tests.Apex/NuGet.Tests.Apex/NuGetIVsApiTests/Utils.cs
@@ -1,8 +1,12 @@
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Threading;
+using NuGet.Common;
 using NuGet.ProjectModel;
 using NuGet.Test.Utility;
+using NuGet.Versioning;
 
 namespace NuGet.Tests.Apex
 {
@@ -18,40 +22,76 @@ namespace NuGet.Tests.Apex
 
         public static bool IsPackageInstalled(NuGetConsoleTestExtension nuGetConsole, string projectPath, string packageName, string packageVersion)
         {
+            var isInstalled = false;
+
             var assetsFile = GetAssetsFilePath(projectPath);
             var packagesConfig = GetPackagesConfigPath(projectPath);
-            if (File.Exists(assetsFile))
+            var packagesConfigExists = File.Exists(packagesConfig);
+
+            if (packagesConfigExists)
             {
-                return PackageExistsInLockFile(assetsFile, packageName, packageVersion);
-            }
-            else if (File.Exists(packagesConfig))
-            {
-                return nuGetConsole.IsPackageInstalled(packageName, packageVersion);
+                isInstalled = nuGetConsole.IsPackageInstalled(packageName, packageVersion);
             }
             else
             {
-                return false;
+                isInstalled = PackageExistsInLockFile(assetsFile, packageName, packageVersion);
             }
+
+            return isInstalled;
         }
 
         private static bool PackageExistsInLockFile(string pathToAssetsFile, string packageName, string packageVersion)
         {
-            var lockFile = new LockFileFormat().Read(pathToAssetsFile);
-            var lockFileLibrary = lockFile.Libraries.SingleOrDefault(p => (String.Compare(p.Name, packageName, StringComparison.OrdinalIgnoreCase) == 0));
-            return lockFileLibrary !=null && lockFileLibrary.Version.ToNormalizedString() == packageVersion;
+            var version = NuGetVersion.Parse(packageVersion);
+            var lockFile = GetAssetsFileWithRetry(pathToAssetsFile);
+            var lockFileLibrary = lockFile.Libraries
+                .SingleOrDefault(p => StringComparer.OrdinalIgnoreCase.Equals(p.Name, packageName)
+                                    && p.Version.Equals(version));
+
+            return lockFileLibrary != null;
+        }
+
+        private static LockFile GetAssetsFileWithRetry(string path)
+        {
+            var timeout = TimeSpan.FromSeconds(10);
+            var timer = Stopwatch.StartNew();
+            string content = null;
+
+            do
+            {
+                if (File.Exists(path))
+                {
+                    try
+                    {
+                        content = File.ReadAllText(path);
+                        var format = new LockFileFormat();
+                        return format.Parse(content, path);
+                    }
+                    catch
+                    {
+                        // Ignore errors from conflicting writes.
+                    }
+                }
+
+                Thread.Sleep(100);
+            }
+            while (timer.Elapsed < timeout);
+
+            // File cannot be read
+            if (File.Exists(path))
+            {
+                throw new InvalidOperationException("Unable to read: " + path);
+            }
+            else
+            {
+                throw new FileNotFoundException("Not found: " + path);
+            }
         }
 
         private static string GetAssetsFilePath(string projectPath)
         {
-            if(string.IsNullOrEmpty(projectPath))
-            {
-                return string.Empty;
-            }
-            else
-            {
-                var projectDirectory = Path.GetDirectoryName(projectPath);
-                return Path.Combine(projectDirectory, "obj", "project.assets.json");
-            }
+            var projectDirectory = Path.GetDirectoryName(projectPath);
+            return Path.Combine(projectDirectory, "obj", "project.assets.json");
         }
 
         private static string GetPackagesConfigPath(string projectPath)
